c567297e4fe5bf786bd0172e2dcf58250ce052fb
Chugreev Kirill <wern587@gmail.com>
2024-09-29 22:03:27 +0300

    Добавьте файлы проекта.

---
+++ PythonApplication2/PythonApplication2.py
@@ -0,0 +1,25 @@
+a=5
+b=10
+
+c = a + b 
+d = a - b 
+e = a * b 
+f = a / b 
+g = a // b 
+h = a % b 
+i = a ** b 
+
+
+print("a + b =", c)
+print("a - b =", d)
+print("a * b =", e)
+print("a / b =", f)
+print("a // b =", g)
+print("a % b =", h)
+print("a ** b =", i)
+print("a > b:", a > b)
+print("a < b:", a < b) 
+print("a == b:", a == b) 
+print("a != b:", a != b)
+print("a >= b:", a >= b) 
+print("a <= b:", a <= b) 
